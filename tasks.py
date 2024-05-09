869221dc715f419749e69c5814d2be07cbc5694c
hungtq <tranquochungqbh@gmail.com>
2024-05-09 15:07:29 +0700

    update command shell robot

--- tasks.py
+++ tasks.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import time
 from robocorp.tasks import task
 from RPA.Browser.Selenium import Selenium
+from DOP.RPA.Log import Log
 
+log = Log()
 selenium = Selenium()
 
 
@@ -11,4 +13,5 @@ selenium = Selenium()
 def resover_data_collected():
     selenium.open_available_browser(url="https://github.com/robocorp/template-python-browser/blob/master/tasks.py")
     time.sleep(5)
+    log.log_info("Mở thành công browser")
     selenium.close_browser()
